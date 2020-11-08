01010d197e4fe1b1ec1012057c6680312b8e5673
Perry Theobald <perrytjr@gmail.com>
2020-11-08 15:01:52 -0800

    api code added

---
+++ employee-directory/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Wikipedia API
+
+export default {
+  get: function(query) {
+    return axios.get(
+      "https://randomuser.me/api/?results=200&nat=us"
+    );
+  }
+};
