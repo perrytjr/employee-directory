ccc74a2dd2b8f8a28d24b96f92c06eb3532c25f5
Perry Theobald <perrytjr@gmail.com>
2020-11-09 14:27:32 -0800

    start of header and search form files

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -4,8 +4,6 @@ import axios from "axios";
 
 export default {
   get: function() {
-    return axios.get(
-      "https://randomuser.me/api/?results=200&nat=us"
-    );
+    return axios.get("https://randomuser.me/api/?results=200&nat=us")
   }
 };
