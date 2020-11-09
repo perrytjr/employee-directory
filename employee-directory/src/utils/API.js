414e89dd2fdc7405a96b77eb06a1742935ee04d7
Perry Theobald <perrytjr@gmail.com>
2020-11-09 11:14:34 -0800

    added component folder

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Wikipedia API
 
 export default {
-  get: function(query) {
+  get: function() {
     return axios.get(
       "https://randomuser.me/api/?results=200&nat=us"
     );
